799c62fa9e866e5755f85dc9c901f54de6ff6d47
Gabe Heath <gabe.d.heath@gmail.com>
2017-05-07 19:27:20 -0400

    More tests

--- src/core/lobby.js
+++ src/core/lobby.js
@@ -1,9 +1,20 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
-export function startGame(state, roomCode) {
-
+export function leaveRoom(state, roomCode, uuid) {
+    //return state.deleteIn(['rooms', roomCode, 'players', 'allPlayers', uuid])
 }
 
-export function setQuestions(state, questions) {
-    return state.set('questions', List(questions));
+export function setQuestions(state, roomCode, questions) {
+    return state.setIn(['rooms', roomCode, 'questions'], Map({
+        currentQuestion: null,
+        questionBank: questions
+    }));
+}
+
+export function startGame(state, roomCode, questions) {
+    if( state.getIn(['rooms', roomCode, 'players', 'allPlayers']).size > 2 ) {
+        const newState = state.setIn(['rooms', roomCode, 'gameInProgress'], true);
+        return setQuestions(newState, roomCode, questions);
+    }
+    return state;
 }
--- test/lobby_spec.js
+++ test/lobby_spec.js
@@ -1,33 +1,76 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
+import uuid from 'uuid';
 
 import {setQuestions, startGame} from '../src/core/lobby';
+import {createRoom, createUniqueRoomCode, joinRoom} from '../src/core/main_menu'
 
 describe('lobby application logic', () => {
 
+    const state = Map({
+        rooms: Map()
+    });
+
+    const player1 = Map({
+        uuid: uuid.v1(),
+        name: 'Player 1'
+    });
+
+    const player2 = Map({
+        uuid: uuid.v1(),
+        name: 'Player 2'
+    });
+
+    const player3 = Map({
+        uuid: uuid.v1(),
+        name: 'Player 3'
+    });
+
+    const questions = List.of('Question 1', 'Question 2');
+
+    const roomCode = createUniqueRoomCode(state.get('rooms'));
+    const nextState = createRoom(state, roomCode, player1);
+    const nextState2 = joinRoom(nextState, roomCode, player2);
+
+
     describe('startGame', () => {
+
+        it('sets gameInProgress to true', () => {
+            const nextState3 = joinRoom(nextState2, roomCode, player3);
+            const nextState4 = startGame(nextState3, roomCode, questions);
+            expect(nextState4.getIn(['rooms', roomCode, 'gameInProgress'])).to.equal(true);
+        });
+
+        it('fails to start if less than 3 players', () => {
+            const nextState3 = startGame(nextState2, roomCode);
+            expect(nextState3).to.equal(nextState2);
+        });
 
     });
 
     describe('setQuestions', () => {
 
         it('adds the questions to the state', () => {
-            const state = Map();
-            const questions = List.of('Question 1', 'Question 2');
-            const nextState = setQuestions(state, questions);
-            expect(nextState).to.equal(Map({
-                questions: List.of('Question 1', 'Question 2')
-            }));
+            const nextState3 = joinRoom(nextState2, roomCode, player3);
+            const nextState4 = startGame(nextState3, roomCode, questions);
+            expect(nextState4.getIn(['rooms', roomCode, 'questions', 'questionBank']).size).to.equal(2);
         });
 
-        it('converts to immutable', () => {
-            const state = Map();
-            const questions = ['Question 1', 'Question 2'];
-            const nextState = setQuestions(state, questions);
-            expect(nextState).to.equal(Map({
-                questions: List.of('Question 1', 'Question 2')
-            }));
+    });
+
+
+
+    describe('leaveRoom', () => {
+
+        it('removes player from list of players in room', () => {
+
         });
+
+    });
+
+
+
+    describe('destroyRoom', () => {
 
     });
 
