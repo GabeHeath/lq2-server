0a76936c2f5cffc52f11011fb5ae32f028beaedf
Gabe Heath <gabe.d.heath@gmail.com>
2017-05-09 00:01:30 -0400

    Added more game tests

--- src/core/game.js
+++ src/core/game.js
@@ -1,3 +1,12 @@
-/**
- * Created by gabeheath on 5/8/17.
- */
+export function increaseScore(state, roomCode, player) {
+    return state.setIn(['rooms', roomCode, 'players','allPlayers', player.get('uuid'), 'score'], parseInt(state.getIn(['rooms', roomCode, 'players', 'allPlayers', player.get('uuid'), 'score'])) + player.get('score'));
+}
+
+export function nextPlayer(state, roomCode) {
+    const players = state.getIn(['rooms', roomCode, 'players']);
+    return state.setIn(['rooms', roomCode, 'players', 'currentPlayer'], (players.get('currentPlayer') % players.get('allPlayers').size) + 1 );
+}
+
+export function submitResponse(state, roomCode, player) {
+    return state.setIn(['rooms', roomCode, 'players', 'allPlayers', player.get('uuid'), 'lastResponse'], player.get('response'));
+}
--- test/core/game_spec.js
+++ test/core/game_spec.js
@@ -2,7 +2,31 @@ import {Map} from 'immutable';
 import {expect} from 'chai';
 import uuid from 'uuid';
 
+import {increaseScore, nextPlayer, submitResponse} from '../../src/core/game'
+import {createRoom, createUniqueRoomCode, joinRoom} from '../../src/core/main_menu'
+
 describe('game application logic', () => {
+
+    const state = Map({
+        rooms: Map()
+    });
+
+    const player1 = Map({
+        uuid: uuid.v1(),
+        name: 'Player 1',
+        score: 10,
+        response: "This is the player's response"
+    });
+
+    const player2 = Map({
+        uuid: uuid.v1(),
+        name: 'Player 2',
+        score: 0
+    });
+
+    const roomCode = createUniqueRoomCode(state.get('rooms'));
+    const nextState = createRoom(state, roomCode, player1);
+    const nextState2 = joinRoom(nextState, roomCode, player2);
 
     describe('generateQuestions', () => {
 
@@ -26,7 +50,10 @@ describe('game application logic', () => {
 
     describe('submitResponse', () => {
 
-        it('sets lastResponse to response');
+        it('sets lastResponse to response', () => {
+            const nextState3 = submitResponse(state, roomCode, player1);
+            expect(nextState3.getIn(['rooms', roomCode, 'players', 'allPlayers', player1.get('uuid'), 'lastResponse'])).to.equal(player1.get('response'));
+        });
 
     });
 
@@ -34,10 +61,36 @@ describe('game application logic', () => {
 
     describe('changePlayer', () => {
 
-        it('changes the current player to the next one in line');
+        it('changes the current player to the next one in line', () => {
+            const nextState3 = nextPlayer(nextState2, roomCode);
+            expect(nextState3.getIn(['rooms', roomCode, 'players', 'currentPlayer'])).to.equal(2);
+        });
 
-        it('wraps back to the first player if current player is the last in line');
-        
+        it('wraps back to the first player if current player is the last in line', () => {
+            const nextState3 = nextPlayer(nextState2, roomCode);
+            const nextState4 = nextPlayer(nextState3, roomCode);
+            expect(nextState4.getIn(['rooms', roomCode, 'players', 'currentPlayer'])).to.equal(1);
+        });
+
+    });
+
+
+
+    describe('increaseScore', () => {
+
+        it('increases the score of a player', () => {
+            const nextState3 = increaseScore(nextState2, roomCode, player1);
+            const nextState4 = increaseScore(nextState3, roomCode, player1);
+            expect(nextState4.getIn(['rooms', roomCode, 'players', 'allPlayers', player1.get('uuid'), 'score'])).to.equal(20);
+        });
+
+    });
+
+
+
+    describe('increaseLikes', () => {
+
+        it('increases the likes for a player');
 
     });
 
